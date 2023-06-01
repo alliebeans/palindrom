a3e662bdbfb4efe19593a00ac065d30c57962be6
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-06-01 10:47:18 +0200

    Formats .js

--- palindrom.js
+++ palindrom.js
@@ -46,52 +46,52 @@ window.onload = function(){
 	const allTd = document.querySelectorAll("td");
 
 	function markLetter(ev) {
-  ev.preventDefault();
+		ev.preventDefault();
 
-  for (i = 0;i < grid.length;i++){
-		for (j = 0;j < grid.length;j++){
-			if (ev.target == grid[j][i]){
-				if (j == 2 || i == 2) {
-					printVertical(grid, 2);
-					printHorizontal(grid, 2);
-				}
-				else if (j == 0 && i != 0 && i != 4) {
-						printVertical(grid, i);
-						printHorizontal(grid, j+1);
-				}
-				else if (j == 4 && i != 0 && i != 4) {
-						printVertical(grid, i);
-						printHorizontal(grid, j-1);
-				}
-				else if (i == 0 && j != 0 && j != 4) {
-						printVertical(grid, i+1);
-						printHorizontal(grid, j);
-				}
-				else if (i == 4 && j != 0 && j != 4) {
-						printVertical(grid, i-1);
-						printHorizontal(grid, j);
-				}
-				else {
-					printVertical(grid, i);
-					printHorizontal(grid, j);
+		for (i = 0;i < grid.length;i++){
+				for (j = 0;j < grid.length;j++){
+					if (ev.target == grid[j][i]){
+						if (j == 2 || i == 2) {
+							printVertical(grid, 2);
+							printHorizontal(grid, 2);
+						}
+						else if (j == 0 && i != 0 && i != 4) {
+								printVertical(grid, i);
+								printHorizontal(grid, j+1);
+						}
+						else if (j == 4 && i != 0 && i != 4) {
+								printVertical(grid, i);
+								printHorizontal(grid, j-1);
+						}
+						else if (i == 0 && j != 0 && j != 4) {
+								printVertical(grid, i+1);
+								printHorizontal(grid, j);
+						}
+						else if (i == 4 && j != 0 && j != 4) {
+								printVertical(grid, i-1);
+								printHorizontal(grid, j);
+						}
+						else {
+							printVertical(grid, i);
+							printHorizontal(grid, j);
+						}
+					}
 				}
 			}
+	}
+
+	function printVertical(grid, i) {
+		for (k = 0;k < grid.length;k++) {
+			grid[k][i].setAttribute("class", "marked");
+		}
+
+	}
+
+	function printHorizontal(grid, j) {
+		for (k = 0;k < grid.length;k++) {
+			grid[j][k].setAttribute("class", "marked");
 		}
 	}
-}
-
-function printVertical(grid, i) {
-	for (k = 0;k < grid.length;k++) {
-		grid[k][i].setAttribute("class", "marked");
-	}
-
-}
-
-function printHorizontal(grid, j) {
-	for (k = 0;k < grid.length;k++) {
-		grid[j][k].setAttribute("class", "marked");
-	}
-}
 
 	function unMarkLetter(){
 		allTd.forEach((td)=>{
